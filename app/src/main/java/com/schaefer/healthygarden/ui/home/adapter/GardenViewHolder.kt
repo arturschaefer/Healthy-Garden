36f815701e6435e2d6ed8f677832a8d6209bf7e3
Artur Schaefer <artur.schaefer2@gmail.com>
2020-06-03 00:46:23 -0300

    changes to glide

--- app/src/main/java/com/schaefer/healthygarden/ui/home/adapter/GardenViewHolder.kt
+++ app/src/main/java/com/schaefer/healthygarden/ui/home/adapter/GardenViewHolder.kt
@@ -7,9 +7,11 @@ import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import coil.api.load
 import coil.transform.CircleCropTransformation
+import com.bumptech.glide.Glide
 import com.schaefer.healthygarden.R
 import com.schaefer.healthygarden.domain.model.GardenDTO
 import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.fragment_profile.*
 import kotlinx.android.synthetic.main.item_home.*
 
 
@@ -19,11 +21,11 @@ class GardenViewHolder(override val containerView: View) :
 
     fun bind(garden: GardenDTO) {
         tvTitle.text = garden.name
-        ivGarden.load(garden.imageUrl) {
-            crossfade(true)
-            placeholder(R.drawable.placeholder_header_garden)
-            transformations(CircleCropTransformation())
-        }
+        Glide.with(containerView)
+            .load(garden.imageUrl)
+            .placeholder(R.drawable.placeholder_header_garden)
+            .error(R.drawable.placeholder_header_garden)
+            .into(ivGarden)
         cvGardenItem.setOnClickListener { view ->
             view.findNavController().navigate(R.id.action_homeFragment_to_detailsGardenFragment)
         }
